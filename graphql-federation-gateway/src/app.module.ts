cd15e293ef3d166d3488a0b64acc30576e53d19e
unknown <dushanse@fortude.co>
2024-10-06 12:21:11 +0530

    init commit

---
+++ graphql-federation-gateway/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { IntrospectAndCompose } from '@apollo/gateway';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      // server: {
+      //   cors: true,
+      // },
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            { name: 'employees', url: 'http://localhost:3000/graphql' },
+            { name: 'projects', url: 'http://localhost:3001/graphql' },
+            { name: 'locations', url: 'http://localhost:3002/graphql' },
+          ],
+        }),
+      },
+    }),
+  ],
+})
+export class AppModule {}
---
+++ project-service/src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { join } from 'path';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AppResolver } from './app.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'employee',
+      entities: ["dist/**/*.entity{.ts,.js}"],
+      synchronize: true,
+    })
+  ],
+  providers: [AppResolver],
+})
+export class AppModule {}
